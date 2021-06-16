941b7f0b3ef7af6f6084e902ed77f38b2a0eb43a
Nurlan <meiramnurlan13@gmail.com>
2021-06-16 15:25:26 +0600

    make variables global

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function computerPlay() {
     let arr = ["Rock", "Paper", "Scissors"];
     let random = Math.floor(Math.random() * 3);
@@ -10,40 +13,39 @@ function playRound(playerSelection, computerSelection) {
     if (player == computer) {
         return "Tie";
     } else if (player == "rock" && computer == "paper") {
+        computerScore++;
         return "You Lose! Paper beats Rock";
     } else if (player == "rock" && computer == "scissors") {
+        playerScore++;
         return "You Win! Rock beats Scissors";
     } else if (player == "paper" && computer == "scissors") {
+        computerScore++;
         return "You Lose! Scissors beats paper";
     } else if (player == "paper" && computer == "rock") {
+        playerScore++;
         return "You Win! Paper beats Rock";
     } else if (player == "scissors" && computer == "rock") {
+        computerScore++;
         return "You Lose! Rock beats Scissors";
     } else if (player == "scissors" && computer == "paper") {
+        playerScore++;
         return "You Win! Scissors beats Paper";
     }
 }
 
 function game() {
-    let playerScore = 0;
-    let computerScore = 0;
     let message = "";
     for (let i = 0; i < 5; i++) {
         let playerSelection = prompt("Please enter rock, paper or scissors")
         let computerSelection = computerPlay();
         message = playRound(playerSelection, computerSelection);
         console.log(message);
-        if (message != "Tie" && message!=undefined){
-            let str=message.substr(0,8);
-            if(str=="You Win!")playerScore++;
-            else if(str=="You Lose")computerScore++;
-        }
     }
     if (playerScore > computerScore) {
         console.log("Congratulations, You won the game!")
     } else if (playerScore < computerScore) {
         console.log("You lost the game, try again")
     } else {
-        console.log("It's tie, try again");
+        console.log("It's a tie, try again");
     }
 }
