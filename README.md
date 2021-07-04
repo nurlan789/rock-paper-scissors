b559c6da402b2c3711be80b2d58866e15338bb7a
Nurlan <meiramnurlan13@gmail.com>
2021-07-04 22:45:58 +0600

    added UI

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-Rock paper scissors game where you play against the computer.
-It has no GUI yet. For now you can play it from the console, just call the game() function.
+Rock-paper-scissors game
--- main.js
+++ main.js
@@ -1,5 +1,17 @@
+const buttons = document.getElementsByTagName('button');
+const result = document.querySelector('#result');
+
+let buttonsCount = buttons.length;
+for (let i = 0; i < buttonsCount; i++) {
+    buttons[i].onclick = function (e) {
+        playerSelection = this.id;
+        game();
+    }
+}
+
 let playerScore = 0;
 let computerScore = 0;
+let playerSelection;
 
 function computerPlay() {
     let arr = ["Rock", "Paper", "Scissors"];
@@ -11,41 +23,50 @@ function playRound(playerSelection, computerSelection) {
     let player = playerSelection.toLowerCase();
     let computer = computerSelection.toLowerCase();
     if (player == computer) {
-        return "Tie";
+        return "It's a tie";
     } else if (player == "rock" && computer == "paper") {
         computerScore++;
-        return "You Lose! Paper beats Rock";
+        return "You Lost! Paper beats Rock";
     } else if (player == "rock" && computer == "scissors") {
         playerScore++;
-        return "You Win! Rock beats Scissors";
+        return "You Won! Rock beats Scissors";
     } else if (player == "paper" && computer == "scissors") {
         computerScore++;
-        return "You Lose! Scissors beats paper";
+        return "You Lost! Scissors beats Paper";
     } else if (player == "paper" && computer == "rock") {
         playerScore++;
-        return "You Win! Paper beats Rock";
+        return "You Won! Paper beats Rock";
     } else if (player == "scissors" && computer == "rock") {
         computerScore++;
-        return "You Lose! Rock beats Scissors";
+        return "You Lost! Rock beats Scissors";
     } else if (player == "scissors" && computer == "paper") {
         playerScore++;
-        return "You Win! Scissors beats Paper";
+        return "You Won! Scissors beats Paper";
     }
 }
 
 function game() {
+    let computerSelection = computerPlay();
     let message = "";
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Please enter rock, paper or scissors")
-        let computerSelection = computerPlay();
-        message = playRound(playerSelection, computerSelection);
-        console.log(message);
-    }
-    if (playerScore > computerScore) {
-        console.log("Congratulations, You won the game!")
-    } else if (playerScore < computerScore) {
-        console.log("You lost the game, try again")
-    } else {
-        console.log("It's a tie, try again");
+    message = playRound(playerSelection, computerSelection);
+    result.innerHTML = message;
+
+    let para = document.createElement('p');
+    let text = document.createTextNode(playerScore + " - " + computerScore);
+    para.appendChild(text);
+    result.appendChild(para);
+
+    if (playerScore == 5 || computerScore == 5) {
+        let finalPara = document.createElement('h2')
+        let finalMessage = "";
+        if (playerScore > computerScore) {
+            finalMessage = document.createTextNode("You won the game!");
+        } else if (playerScore < computerScore) {
+            finalMessage = document.createTextNode("You lost the game");
+        }
+        finalPara.appendChild(finalMessage);
+        result.appendChild(finalPara);
+        playerScore = 0;
+        computerScore = 0;
     }
 }
